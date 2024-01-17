83575d14735f62028b4daa11b27c3e54f2e11c88
Herr Prof <121493590+Herr-Professor@users.noreply.github.com>
2024-01-17 01:23:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById("my-form");
+  const emergencyForm = document.getElementById("emergencyForm");
 
   async function handleSubmit(event) {
     event.preventDefault();
+
     const status = document.getElementById("my-form-status");
-    const data = new FormData(event.target);
 
     try {
-      const response = await fetch(event.target.action, {
-        method: form.method,
-        body: data,
-        headers: {
-          'Accept': 'application/json'
-        }
-      });
-
-      if (response.ok) {
-        status.innerHTML = "Thanks for your submission!";
-        form.reset();
-      } else {
-        const responseData = await response.json();
-        if (responseData.errors) {
-          status.innerHTML = responseData.errors.map(error => error.message).join(", ");
-        } else {
-          status.innerHTML = "Oops! There was a problem submitting your form.";
-        }
-      }
+      await formspree.submitForm(emergencyForm);
+      status.innerHTML = "Form submitted successfully!";
+      emergencyForm.reset();
     } catch (error) {
       status.innerHTML = "Oops! There was a problem submitting your form.";
     }
   }
 
-  form.addEventListener("submit", handleSubmit);
+  emergencyForm.addEventListener("submit", handleSubmit);
 });
